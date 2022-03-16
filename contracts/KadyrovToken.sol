a50f03492cc32ac9a02be4feadba313440b7f824
Arystan <a_r_s_t_a_n@mail.ru>
2022-03-16 18:03:01 +0700

    transfer&transferFrom

--- contracts/KadyrovToken.sol
+++ contracts/KadyrovToken.sol
@@ -15,29 +15,12 @@ contract KadyrovToken {
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
-    // function name() public view returns (string) {
-    //     return name;
-    // }
-
-    // function symbol() public view returns (string) {
-    //     return symbol;
-    // }                                                    Solidity уже создал для этих полей свои публичные геттеры, верно?
-
-    // function decimals() public view returns (uint8) {
-    //     return decimals;
-    // }
-
-    // function totalSupply() public view returns (uint256) {
-    //     return totalSupply;
-    // }
-
     function balanceOf(address _owner) public view returns (uint256 balance) {
         return _balances[_owner];
     }
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(_value < _balances[msg.sender], "Withdrawal value is greater than total balance");
-        // _to.transfer(_value); Что будет если сделать address payable и оптравлять через transfer?
+        require(_value < _balances[msg.sender], "Caller's account balance doesn't have enough tokens");
         _balances[msg.sender] -= _value;
         _balances[_to] += _value;
         emit Transfer(msg.sender, _to, _value);
@@ -45,7 +28,11 @@ contract KadyrovToken {
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-
+        require(_value < _balances[_from], "Not enough tokens on the balance to make transfer");
+        _balances[_from] -= _value;
+        _balances[_to] += _value;
+        emit Transfer(_from, _to, _value);
+        return true;
     }
 
     function approve(address _spender, uint256 _value) public returns (bool success) {
