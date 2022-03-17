919d179cbd796cae30f0553b94be350f4e91bcb8
Arystan <a_r_s_t_a_n@mail.ru>
2022-03-17 12:07:13 +0700

    Finished standard methods
    Finished approve, allowance. Made approve and transferFrom safe from attack vector

--- contracts/KadyrovToken.sol
+++ contracts/KadyrovToken.sol
@@ -13,7 +13,7 @@ contract KadyrovToken {
     mapping(address => mapping(address => uint256)) public _allowances;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _oldValue, uint256 _value); // ask about transfer with four arguments (is it override?)
 
     function balanceOf(address _owner) public view returns (uint256 balance) {
         return _balances[_owner];
@@ -21,25 +21,34 @@ contract KadyrovToken {
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
         require(_value < _balances[msg.sender], "Caller's account balance doesn't have enough tokens");
+        require(_to != address(0), "_to shouldn't be 0x0 address");
         _balances[msg.sender] -= _value;
         _balances[_to] += _value;
         emit Transfer(msg.sender, _to, _value);
         return true;
     }
-
+    
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_to != address(0), "_to shouldn't be 0x0 address");
         require(_value < _balances[_from], "Not enough tokens on the balance to make transfer");
+        require(_allowances[_from][msg.sender] >= _value, "Not enough allowance");
         _balances[_from] -= _value;
+        _allowances[_from][msg.sender] -= _value;
         _balances[_to] += _value;
         emit Transfer(_from, _to, _value);
         return true;
     }
 
-    function approve(address _spender, uint256 _value) public returns (bool success) {
-
+    function approve(address _spender, uint256 _currentValue, uint256 _value) public returns (bool success) {
+        require(_spender != address(0), "_spender cannot be the zero address");
+        if (_allowances[msg.sender][_spender] == _currentValue) { // condition to prevent attack vector
+            _allowances[msg.sender][_spender] += _value;
+            emit Approval(msg.sender, _spender, _currentValue, _value);
+            return true;
+        } else { return false; }
     }
 
     function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
-        
+        return _allowances[_owner][_spender];
     }
 }
