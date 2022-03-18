7f5bd82b747ee2b70edf435c175cbc21063220fc
Arystan <a_r_s_t_a_n@mail.ru>
2022-03-18 18:30:46 +0700

    Tests for transferFrom&Approve

--- test/sample-test.ts
+++ test/sample-test.ts
@@ -68,17 +68,83 @@ describe("Token", function () {
     });
 
     it("Transfer to zero address should revert", async() => {
-      await expect(contract.transfer(ethers.constants.AddressZero, 1)).to.be.revertedWith("_to shouldn't be 0x0 address");
+      expect(contract.transfer(ethers.constants.AddressZero, 1)).to.be.revertedWith("_to shouldn't be 0x0 address");
     });
 
     it("Transfer of more tokens that user have should revert", async() => {
-      await expect(contract.transfer(addr1.address, 100000000000)).to.be.revertedWith("Caller's account balance doesn't have enough tokens");
+      expect(contract.transfer(addr1.address, 100000000000)).to.be.revertedWith("Caller's account balance doesn't have enough tokens");
     });
 
     it("Transfer event should be emitted", async() => {
-      await expect(contract.transfer(addr1.address, 1)).to.be.emit(contract, "Transfer");
+      expect(contract.transfer(addr1.address, 1)).to.be.emit(contract, "Transfer");
+    });
+  });
+
+  describe("TransferFrom", () => {
+    it("Correct transferFrom call should change balanceOf and allowances addresses", async () => {
+      let ownerBalanceBefore = await contract.balanceOf(owner.address);
+      let tokensToSend = 6;
+      let approvedTokens = 10;
+      await contract.approve(addr1.address, 0, approvedTokens);
+      await contract.connect(addr1).transferFrom(owner.address, addr1.address, tokensToSend);
+
+      expect(await contract.balanceOf(owner.address)).to.equal(ownerBalanceBefore - tokensToSend);
+      expect(await contract.balanceOf(addr1.address)).to.equal(tokensToSend);
+      expect(await contract.allowance(owner.address, addr1.address)).to.equal(approvedTokens - tokensToSend);
     });
 
+    it("TransferFrom with no balance shouldn't change balanceOf addresses", async() => {
+      let tokensToSend = 6;
+      let approvedTokens = 10;
+      await contract.approve(addr1.address, 0, approvedTokens);
+      expect(contract.transferFrom(addr1.address, owner.address, tokensToSend)).to.be.revertedWith("Not enough tokens on the balance to make transfer");
+    });
+
+    it("TransferFrom to zero address shouldn't work", async () => {
+      let tokensToSend = 6;
+      let approvedTokens = 10;
+      await contract.approve(addr1.address, 0, approvedTokens);
+      expect(contract.transferFrom(addr1.address, ethers.constants.AddressZero, tokensToSend)).to.be.revertedWith("_to shouldn't be 0x0 address");
+    });
+
+    it("TransferFrom with not enough allowance shouldn't work", async () => {
+      let tokensToSend = 11;
+      let approvedTokens = 10;
+      await contract.approve(addr1.address, 0, approvedTokens);
+      expect(contract.transferFrom(addr1.address, owner.address, tokensToSend)).to.be.revertedWith("Not enough allowance");
+    });
+
+    it("TransferFrom should emit Transfer event", async () => {
+      let tokensToSend = 5;
+      let approvedTokens = 10;
+      await contract.approve(addr1.address, 0, approvedTokens);
+      expect(contract.transferFrom(addr1.address, owner.address, tokensToSend)).to.be.emit(contract, "Transfer");
+    });
+  });
+
+  describe("approve", () => {
+    it("Approve to zero address shouldn't work", async () => {
+      expect(contract.approve(ethers.constants.AddressZero, 0, 10)).to.be.revertedWith("_spender cannot be the zero address");
+    });
+
+    it("Approve should emit Approval event", async () => {
+      expect(contract.approve(addr1.address, 0, 10)).to.be.emit(contract, "Approval");
+    });
+
+    it("Approve should emit Approval event", async () => {
+      expect(contract.approve(addr1.address, 0, 10)).to.be.emit(contract, "Approval");
+    });
+
+    it("Approve should increase balance _allowances", async () => {
+      await contract.approve(addr1.address, 0, 10);
+      expect(await contract.allowance(owner.address, addr1.address)).to.equal(10);
+    });
+
+    it("Approve should decrease balance _allowances", async () => {
+      await contract.approve(addr1.address, 0, 10);
+      await contract.connect(addr1).transferFrom(owner.address, addr1.address, 5);
+      expect(await contract.allowance(owner.address, addr1.address)).to.equal(5);
+    });
   });
 
 });
